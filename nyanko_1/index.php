14b441d0a44722bb1c26b373dd21eb6cdb0a961e
Takayuki <6015abc@gmail.com>
2020-12-03 17:22:02 +0900

    initial commit

---
+++ nyanko_1/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>きょうのにゃんこ</title>
+  <link rel="stylesheet" href="./common/css/reset/reset.css" />
+  <link rel="stylesheet" href="./common/css/style.css" />
+</head>
+
+<body>
+  <!-- ヘッダーエリア -->
+  <header id="#header" class="header">
+    <?php
+    include(dirname(__FILE__) . '/include/nhk-header.html');
+    include(dirname(__FILE__) . '/include/header.html');
+    ?>
+  </header>
+
+  <main>
+
+  </main>
+</body>
+
+</html>
