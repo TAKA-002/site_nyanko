1b173ab019872352619f742ed593d01f52412a5d
Takayuki <6015abc@gmail.com>
2020-12-08 20:05:47 +0900

    整形：20201208

--- nyanko_3/js/main.js
+++ nyanko_3/js/main.js
@@ -1,11 +1,9 @@
 $(document).ready(function () {
-    //ボタンの「＋」記号を回転
-    $('.icon').mouseover(function () {
-        $(this).addClass('rotate');
-    });
-    $('.icon').mouseleave(function () {
-        $(this).removeClass('rotate');
-    });
-
-
+  //ボタンの「＋」記号を回転
+  $(".icon").mouseover(function () {
+    $(this).addClass("rotate");
+  });
+  $(".icon").mouseleave(function () {
+    $(this).removeClass("rotate");
+  });
 });
